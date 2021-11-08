55c39c0b2f26db947251d740975042ccc913f00b
Keem Codes <akeeminbox@gmail.com>
2021-11-08 16:41:25 -0500

    first code commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+ 
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="/images/logo.svg" alt="">
+        </div>
+        <div class="hamburger-menu">
+            <img src="/images/menu.svg" alt="">
+        </div>
+    </header>
+    <main>
+        <section class="home">
+            <!-- Home Page -->
+            <div class="page-title">Hello World!</div>
+            <div class="page-photo-icon"></div>
+            <div class="page-intro">My name is Keem</div>
+            <div class="page-description">I design and build apps</div>
+            <div class="page-button"><button>REACH ME</button></div>
+        </section>
+        <section>
+            <!-- About Me  -->
+            <div class="heading-title"></div>
+            <div class="section-title"></div>
+            <div class="section-body"></div>
+            <div class="section-languages"></div>
+        </section>
+        <section>
+            <!-- Projects -->
+            <div class="heading-title"></div>
+
+        </section>
+        <section>
+            <!-- Employment -->
+            <div class="heading-title"></div>
+
+        </section>
+        <section>
+            <!-- Contact Me -->
+            <div class="heading-title"></div>
+
+        </section>
+    </main>
+</body>
+</html>
